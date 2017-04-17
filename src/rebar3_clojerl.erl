b6c5bfedf5eccbf6568c593d0793d5da8ddfbb23
Juan Facorro <juan.facorro@klarna.com>
2017-04-17 12:42:36 +0200

    Update description and usage

--- src/rebar3_clojerl.erl
+++ src/rebar3_clojerl.erl
@@ -4,4 +4,5 @@
 
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
-  {ok, _State1} = rebar3_clojerl_compile_prv:init(State).
+  {ok, State1}  = rebar3_clojerl_compile_prv:init(State),
+  {ok, _State2} = rebar3_clojerl_repl_prv:init(State1).
