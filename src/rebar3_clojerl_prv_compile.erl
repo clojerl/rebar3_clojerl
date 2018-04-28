f67e71c734b4b25b781ba23e96ae0f7bf8e8e96a
Juan Facorro <juan.facorro@klarna.com>
2018-04-28 13:46:27 +0200

    [#20] Abort on compiler error

--- src/rebar3_clojerl_prv_compile.erl
+++ src/rebar3_clojerl_prv_compile.erl
@@ -7,7 +7,7 @@
 -define(DEPS, [{default, app_discovery}, {default, compile}]).
 -define(DEFAULT_SRC_DIRS, ["src"]).
 
--type filename() :: filename:filename_all().
+-define(CLOJERL, <<"clojerl">>).
 
 %% =============================================================================
 %% Public API
@@ -30,14 +30,14 @@ init(State) ->
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
   ok     = ensure_clojerl(State),
-  Apps   = lists:filter(is_not_dep_name_fun(<<"clojerl">>), all_apps(State)),
+  Apps   = lists:filter(is_not_dep_name_fun(?CLOJERL), all_apps(State)),
   Config = [{protocols_out_dir, protocols_out_dir(State)}],
   [compile(AppInfo, Config) || AppInfo <- Apps],
   {ok, State}.
 
--spec format_error(any()) ->  iolist().
+-spec format_error(any()) -> iolist().
 format_error(Reason) ->
-  io_lib:format("~p ~p", [Reason, erlang:get_stacktrace()]).
+  io_lib:format("~p", [Reason]).
 
 %% =============================================================================
 %% Internal functions
@@ -47,23 +47,23 @@ format_error(Reason) ->
 all_apps(State) ->
   lists:usort(rebar_state:all_deps(State)) ++ rebar_state:project_apps(State).
 
--spec protocols_out_dir(rebar_state:t()) -> filename().
+-spec protocols_out_dir(rebar_state:t()) -> file:name().
 protocols_out_dir(State) ->
   %% Get the first application from this project.
   [AppInfo | _] = rebar_state:project_apps(State),
-  BaseDir       = rebar_app_info:out_dir(AppInfo),
-  ProtoDir      = filename:join(BaseDir, <<"ebin">>),
-  rebar_api:debug("Protocols Dir ~s", [ProtoDir]),
-  ProtoDir.
+  ProtoDir      = rebar_app_info:ebin_dir(AppInfo),
+  rebar_api:debug("Protocols dir: ~s", [ProtoDir]),
+  list_to_binary(ProtoDir).
 
 -spec ensure_clojerl(rebar_state:t()) -> ok.
 ensure_clojerl(State) ->
-  case find_dep(State, <<"clojerl">>) of
+  case find_dep(State, ?CLOJERL) of
     notfound ->
       rebar_api:abort("Clojerl was not found as a dependency", []);
     {ok, DepInfo} ->
-      EbinDir  = filename:join(rebar_app_info:out_dir(DepInfo), "ebin"),
-      true     = code:add_patha(EbinDir),
+      EbinDir  = rebar_app_info:ebin_dir(DepInfo),
+      rebar_api:debug("Add Clojerl path: ~s", [EbinDir]),
+      true     = code:add_pathz(EbinDir),
       ok       = clojerl:start()
   end.
 
@@ -88,42 +88,45 @@ is_not_dep_name_fun(Name) ->
 compile(AppInfo, Config0) ->
   rebar_api:info("Clojerl Compiling ~s", [rebar_app_info:name(AppInfo)]),
   BaseDir      = rebar_app_info:out_dir(AppInfo),
+  EbinDir      = rebar_app_info:ebin_dir(AppInfo),
   RebarOpts    = rebar_app_info:opts(AppInfo),
   CljeSrcDirs  = rebar_opts:get(RebarOpts, clje_src_dirs, ?DEFAULT_SRC_DIRS),
 
-  OutDir       = filename:join(BaseDir, <<"ebin">>),
-  Config1      = [{out_dir, OutDir} | Config0],
+  rebar_api:debug("Add path: ~s", [EbinDir]),
   %% TODO: ensure dir
-  ok           = code:add_pathsa([OutDir]),
+  ok           = code:add_pathsa([EbinDir]),
 
-  [compile_dir(BaseDir, SrcDir, OutDir, Config1) || SrcDir <- CljeSrcDirs],
+  Config1      = [{out_dir, list_to_binary(EbinDir)} | Config0],
+
+  [compile_dir(BaseDir, SrcDir, EbinDir, Config1) || SrcDir <- CljeSrcDirs],
 
   ok.
 
--spec compile_dir(filename(), string(), filename(), [any()]) -> ok.
-compile_dir(BaseDir, Dir, OutDir, Config) ->
+-spec compile_dir(file:name(), string(), file:name(), [any()]) -> ok.
+compile_dir(BaseDir, Dir, EbinDir, Config) ->
   SrcDir   = filename:join(BaseDir, Dir),
   SrcFiles = find_files(SrcDir),
 
   rebar_api:debug("Source Dir ~s", [SrcDir]),
   rebar_api:debug("Source Files ~p", [SrcFiles]),
-  rebar_api:debug("Out Dir ~s", [OutDir]),
+  rebar_api:debug("Ebin Dir ~s", [EbinDir]),
 
   %% TODO: ensure dir
   true     = code:add_patha(SrcDir),
 
-  [compile_file(Src, SrcDir, OutDir, Config) || Src <- SrcFiles],
+  [compile_file(Src, SrcDir, EbinDir, Config) || Src <- SrcFiles],
   ok.
 
--spec compile_file(filename(), filename(), filename(), [any()]) -> ok | skipped.
-compile_file(Source, SrcDir, OutDir, Config) ->
-  Target = target_file(Source, SrcDir, OutDir),
-  case check_last_modified(Target, Source) of
+-spec compile_file(file:name(), file:name(), file:name(), [any()]) ->
+  ok | skipped.
+compile_file(Source, SrcDir, EbinDir, Config) ->
+  Target = target_file(Source, SrcDir, EbinDir),
+  case should_compile(Target, Source) of
     true  -> compile_clje(Source, Config);
     false -> skipped
   end.
 
--spec compile_clje(filename(), [any()]) -> ok.
+-spec compile_clje(file:name(), [any()]) -> ok.
 compile_clje(Source, Config) ->
   rebar_api:debug("Compiling ~s...", [Source]),
 
@@ -138,21 +141,23 @@ compile_clje(Source, Config) ->
     clj_compiler:compile_file(list_to_binary(Source))
   catch
     _:Reason ->
-      rebar_api:error("~s", [clj_rt:str(Reason)]),
       Stacktrace = erlang:get_stacktrace(),
       rebar_api:debug( "Stacktrace:~n~s"
                      , [clj_utils:stacktrace(Stacktrace)]
+                     ),
+      rebar_api:abort( "Error while compiling ~s: ~s"
+                     , [Source, clj_rt:str(Reason)]
                      )
   after
     ok = 'clojerl.Var':pop_bindings()
   end.
 
--spec find_files(filename()) -> [filename()].
+-spec find_files(file:name()) -> [file:name()].
 find_files(Path) ->
   ExtRegex = "clj[ce]",
   rebar_utils:find_files(Path, ExtRegex, true).
 
--spec target_file(filename(), filename(), filename()) -> filename().
+-spec target_file(file:name(), file:name(), file:name()) -> file:name().
 target_file(Source, SrcDir, OutDir) ->
   Target1   = re:replace(Source, ["^", SrcDir, "/"], "", [global]),
   Target2   = re:replace(Target1, "/", ".", [global, {return, binary}]),
@@ -161,6 +166,7 @@ target_file(Source, SrcDir, OutDir) ->
   Target5   = [OutDir, "/", Target4, ".beam"],
   iolist_to_binary(Target5).
 
--spec check_last_modified(filename(), filename()) -> boolean().
-check_last_modified(Target, Source) ->
-  filelib:last_modified(Target) < filelib:last_modified(Source).
+-spec should_compile(file:name(), file:name()) -> boolean().
+should_compile(Target, Source) ->
+  not filelib:is_file(Target)
+    orelse filelib:last_modified(Target) < filelib:last_modified(Source).
